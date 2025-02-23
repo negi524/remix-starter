f4b76eb305cee9f12778e517f4081b8c00af68d5
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-02-23 20:38:05 +0900

    ユニットテスト環境構築（Vitest） (#9)
    * feat: yarn add -D vitest
    
    * feat: create empty directory
    
    * feat: vitest導入
    
    * feat: upgrade libraries
    
    * feat: add coverage
    
    * feat: coverage setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
@@ -21,4 +21,13 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  test: {
+    globals: true,
+    coverage: {
+      enabled: true,
+      // 対象はappディレクトリ配下
+      include: ["app/**"],
+      reporter: ["html"],
+    },
+  },
 });
