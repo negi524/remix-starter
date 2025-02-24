4aff7752129996e010899164c3faee017a741f1d
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-02-24 14:50:17 +0900

    feat: viteの設定修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
